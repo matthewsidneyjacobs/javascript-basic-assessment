73ae62a58b937e6e1447a7567a3e3a0cfa2db2b7
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-11-11 12:42:16 -0700

    one problem left

--- assessment.js
+++ assessment.js
@@ -43,7 +43,11 @@ myOtherShirt['type'] = 'spandex';
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
-
+var zoo = {
+	"monkeys": 8,
+	"giraffes": 4,
+	"elephants": 2
+}
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
@@ -55,47 +59,96 @@ var snake = {
 	runs: 'legless'
 };
 
+for (var prop in snake) {
+	if (prop[0] === 's') {
+		snake[prop] = 's';
+	}
+}
+
 //#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
+
+	var rainbowColors = ['red', 'orange', 'yellow', 'green','blue', 'indigo', 'violet'];
 
 // #8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 // add 'wonderwoman' to the end
-
+	heroes.push('wonderwoman');
 // remove 'superman' and store him in a variable called firstHero
-
+	var firstHero = heroes.shift();
 // add 'spongebob' to the start of the array
-
+	heroes.unshift('spongebob');
 // remove 'flash' from the array and store him in a variable called secondHero
+	var secondHero = heroes.splice(2,1);
 
 // leave batman in the array but put a copy of him on a variable called thirdHero
 
-
+	var thirdHero = heroes[1];
 
 
 // #9 Write a function called addItem that takes in an array, an item, and adds the item to
 // the array.
-
+	function addItem(array, item) {
+		array.push(item);
+		return array;
+	}
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
-
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
+	function removeItem(array, string) {
+		var modArr= array;
+		return modArr.filter(function(x) {
+			return x !== string;
+		});
+	}
 
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
-
-
+	function doubleTheFun(item) {
+		if (isNaN(item)) {
+			return item+item;
+		}
+		else {
+			return item*2;
+		}
+	}
 
 // #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
 
+	function getValueOfProperty(obj, propName) {
+		for (var prop in obj) {
+			if (prop === propName) {
+				return obj[prop];
+			}
+		}
+	}
 
 // #13 Write a function called makeChatMessage that takes in a message and author as parameters
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
+	function makeChatMessage(message, author) {
+		var obj = {
+			message: message,
+			author: author,
+			timestamp: new Date()
+		}
+		return obj;
+	}
+
 
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
-
+	function coderTest(obj) {
+		if (obj.name === "Jeremy") {
+			obj.lovesCode = 10;
+		}
+		else if (obj.name === "Brack") {
+			obj.lovesCode = 0;
+		}
+		else {
+			obj.lovesCode = 5;
+		}
+	}
 
 // #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
 /*
@@ -105,6 +158,34 @@ var heroes = ['superman', 'batman', 'flash'];
     temperature over 80 or humidity over 50 or cloudiness over 50 - return "Hmm, probably not"
     Otherwise - return "I love outside"
 */
+	function outside(temperature,humidity,cloudiness) {
+		if (temperature > 80 && humidity > 40) {
+			return "I'm all sweat";
+		}
+		else if (temperature < 40 && cloudiness > 60) {
+			return "I have icecicles";
+		}
+		else if (temperature > 80 && humidity < 40 && cloudiness < 20) {
+			return "I'm literally in the desert";
+		}
+		else if (temperature > 80) {
+			return "Hmm, probably not";
+		}
+		else if (humidity > 50) {
+			return "Hmm, probably not";
+		}
+		else if (cloudiness > 50) {
+			return "Hmm, probably not";
+		}
+		else {
+			return "I love outside";
+		}
+	}
 
 // #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
 // example - If I call you with 'Give it' you should invoke holla with 'Give it back'
+
+
+		function callerBack(cb, str) {
+			return cb(str);
+		}
